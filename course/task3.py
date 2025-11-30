f3a0f5f5fa0adf7550cedcabff9d142e25e61b7a
Sanzhar <svbyrs@gmail.com>
2025-11-30 16:18:03 +0500

    Add files via upload

---
+++ course/task3.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, request
+from math import gcd
+
+app = Flask(__name__)
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+@app.route('/svbyrs_gmail_com', methods=['GET'])
+def compute_lcm():
+    try:
+        x = int(request.args.get('x'))
+        y = int(request.args.get('y'))
+        if x < 0 or y < 0:
+            return "NaN"
+        return str(lcm(x, y))
+    except (ValueError, TypeError):
+        return "NaN"
+
+if __name__ == '__main__':
+    # Runs on all interfaces, port 5000
+    app.run(host='0.0.0.0', port=5000)
